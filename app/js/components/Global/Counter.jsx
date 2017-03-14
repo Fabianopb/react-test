1e28426d199f899cb2151ba180bfc3afb6b84020
Fabiano Brito <fabianopbrito@gmail.com>
2017-03-14 21:31:43 +0200

    Creates simple Counter component to display in the home component.

---
+++ app/js/components/Global/Counter.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Counter extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  render() {
+    return (
+      <button onClick={ () => this.setState({ count: this.state.count + 1 }) }>
+        Clicks: { this.state.count }
+      </button>
+    );
+  }
+}
--- app/js/views/Home.jsx
+++ app/js/views/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Counter from '../components/Global/Counter';
 
 export default class Menu extends React.Component {
   render() {
     return (
       <div>
         Home Page
+        <Counter />
       </div>
     );
   }
